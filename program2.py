c939c5a0fc4e052714eb90ff5b0fdf4289b1b501
Yashraj360 <yashraj.sahu.jobsdoor360@gmail.com>
2024-09-30 14:42:53 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -30,8 +30,3 @@ class Solution(object):
 
         return result
 solution = Solution()
-print(solution.romanToInt("III"))   # Output: 3
-print(solution.romanToInt("IV"))    # Output: 4
-print(solution.romanToInt("IX"))    # Output: 9
-print(solution.romanToInt("LVIII"))  # Output: 58
-print(solution.romanToInt("MCMXCIV"))  # Output: 1994
